3dff3087ea721d8668f10e79baca106144c6796a
YuzukiTsuru <gloomyghost@gloomyghost.com>
2022-03-21 14:31:59 +0800

    build: update envsetup

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
-PWD
-
 alias cbr="cd buildroot"
 alias rebuild_kernel="rm ./output/images/*.dtb && make linux-rebuild -j8 && make"
+alias rebuild_uboot="make uboot-rebuild -j8 && make"
+alias wsl_path="export PATH=\"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/wsl/lib\""
+alias sync_kernel="rm -rf output/build/linux* && rm -rf output/build/.linux*"
+
