ef3d2fb45ae92541a23cc241e902df892361fcbc
YuzukiTsuru <gloomyghost@gloomyghost.com>
2022-03-26 12:51:18 +0800

    roll back

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-alias lunch="cd buildroot && echo -e \"You're building on Linux\n\nLunch menu... pick a combo by 'make xxx_defconfig':\" && make list-defconfigs"
+alias lunch="cd buildroot && echo -e \"You're building on Linux\n\nLunch menu... Scaning combo... \npick a combo by 'make xxx_defconfig':\" && make list-defconfigs"
 alias rebuild_kernel="rm ./output/images/*.dtb && make linux-rebuild -j8 && make"
 alias rebuild_uboot="make uboot-rebuild -j8 && make"
 alias wsl_path="export PATH=\"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/wsl/lib\""
