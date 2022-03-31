20ebebe6868a8cbcedc092c98f338353fcbb6c70
YuzukiTsuru <gloomyghost@gloomyghost.com>
2022-04-01 00:21:31 +0800

    fix rebuild kernel dtb not found error
    Signed-off-by: YuzukiTsuru <gloomyghost@gloomyghost.com>

--- envsetup.sh
+++ envsetup.sh
@@ -12,7 +12,7 @@ fi
 
 # Alias
 alias lunch="cd buildroot && echo -e \"You're building on Linux\n\nLunch menu... Scaning combo... \npick a combo by 'make xxx_defconfig':\" && make list-defconfigs"
-alias rebuild_kernel="rm ./output/images/*.dtb && make linux-rebuild -j8 && make"
+alias rebuild_kernel="touch ./output/images/a.dtb && rm ./output/images/*.dtb && make linux-rebuild -j8 && make"
 alias rebuild_uboot="make uboot-rebuild -j8 && make"
 alias wsl_path="export PATH=\"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/wsl/lib\""
 alias sync_kernel="rm -rf output/build/linux* && rm -rf output/build/.linux* && make"
