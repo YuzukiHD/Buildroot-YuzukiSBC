07fcbfd0bf8134886a619390a39093ec482dc29b
YuzukiTsuru <gloomyghost@gloomyghost.com>
2022-04-05 19:50:04 +0800

    envsetup: add gcc version check

--- envsetup.sh
+++ envsetup.sh
@@ -6,12 +6,12 @@
 #
 
 # WSL SUpport 
-if [ $(uname -r | grep -c "WSL1") -eq 1 ]
+if [ $(uname -r | grep -c "WSL1") -eq 1 ];
 then 
     # Not support WSL 1
     echo "#### Buildroot-YuzukiSBC Not Support WSL 1 ####"
     exit 1
-elif [ $(uname -r | grep -c "WSL2") -eq 1 ]
+elif [ $(uname -r | grep -c "WSL2") -eq 1 ];
 then 
     echo "Buildroot-YuzukiSBC Now running on WSL2, setting PATH..."
     export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/wsl/lib"
@@ -25,10 +25,16 @@ MAJOR=$(echo __GNUC__ | $compiler -E -xc - | tail -n 1)
 MINOR=$(echo __GNUC_MINOR__ | $compiler -E -xc - | tail -n 1)
 PATCHLEVEL=$(echo __GNUC_PATCHLEVEL__ | $compiler -E -xc - | tail -n 1)
 
-if [ $MAJOR -lt 7 ]
+if [ $MAJOR -lt 7 ];
 then 
     echo "#### Buildroot-YuzukiSBC Not Support GCC Version less than 7 ####"
     exit 1
+elif [ $MAJOR -gt 12 ];
+then
+    echo "#### Buildroot-YuzukiSBC Not Support GCC Version more than 12 ####"
+    exit 1
+else
+    echo "Your Host GCC Version is $MAJOR.$MINOR.$PATCHLEVEL"
 fi
 
 # Add alias 
