8db6689d675e7e4e8e02d80f1120eece2eb49b65
YuzukiTsuru <gloomyghost@gloomyghost.com>
2022-04-05 15:30:22 +0800

    fix ^1 bugs, support new build env

--- buildroot/board/allwinner-generic/sun20i-generic/script/post-build.sh
+++ buildroot/board/allwinner-generic/sun20i-generic/script/post-build.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
-BOARD_DIR="$(dirname $0)../../$1"
+BOARD_DIR="$(dirname $0)../../$3"
 
-if grep -q ^BR2_MY_LOGIN_SYSTEM=y ${BR2_CONFIG}
-then
-    cp $BOARD_DIR/bin/* -rfvd  $BINARIES_DIR
-    cd $BINARIES_DIR
-    $BINARIES_DIR/dragonsecboot  -pack boot_package.cfg
-    mkbootimg --kernel  Image  --ramdisk  ramdisk.img --board  d1-nezha_min --base  0x40200000 --kernel_offset  0x0 --ramdisk_offset  0x01000000 -o  boot.img
-else
-    # For debug
-    echo $BOARD_DIR
-fi
+# For debug
+echo "Target binary dir $BOARD_DIR"
+
+cp $BOARD_DIR/bin/* -rfvd  $BINARIES_DIR
+cd $BINARIES_DIR
+$BINARIES_DIR/dragonsecboot  -pack boot_package.cfg
+mkbootimg --kernel  Image  --ramdisk  ramdisk.img --board  d1-nezha_min --base  0x40200000 --kernel_offset  0x0 --ramdisk_offset  0x01000000 -o  boot.img
+
