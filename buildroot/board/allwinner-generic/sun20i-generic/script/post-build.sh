7720ff9cb4d181af85efd08fdd45ca174d0c8db7
YuzukiTsuru <gloomyghost@gloomyghost.com>
2022-04-05 20:18:33 +0800

    fix post args

--- buildroot/board/allwinner-generic/sun20i-generic/script/post-build.sh
+++ buildroot/board/allwinner-generic/sun20i-generic/script/post-build.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-BOARD_DIR="$(dirname $0)../../$3"
+BOARD_DIR="$(dirname $0)/../../$4"
 
 # For debug
 echo "Target binary dir $BOARD_DIR"
 
 cp $BOARD_DIR/bin/* -rfvd  $BINARIES_DIR
 cd $BINARIES_DIR
+echo "item=dtb, $5" >> boot_package.cfg
 $BINARIES_DIR/dragonsecboot  -pack boot_package.cfg
 mkbootimg --kernel  Image  --ramdisk  ramdisk.img --board  d1-nezha_min --base  0x40200000 --kernel_offset  0x0 --ramdisk_offset  0x01000000 -o  boot.img
 
