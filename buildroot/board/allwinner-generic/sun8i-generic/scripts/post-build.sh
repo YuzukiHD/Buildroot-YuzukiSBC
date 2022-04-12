37c80768e7675793d7986ab79d7fccd9bca3b7e3
YuzukiTsuru <gloomyghost@gloomyghost.com>
2022-04-12 14:48:51 +0800

    board: support mq dual t113

--- buildroot/board/allwinner-generic/sun8i-generic/scripts/post-build.sh
+++ buildroot/board/allwinner-generic/sun8i-generic/scripts/post-build.sh
@@ -13,4 +13,3 @@ echo "item=dtb, $5" >> boot_package.cfg
 
 $BINARIES_DIR/dragonsecboot  -pack boot_package.cfg
 mkbootimg --kernel zImage  --ramdisk  ramdisk.img --board sun8iw20p1 --base  0x40200000 --kernel_offset  0x0 --ramdisk_offset  0x01000000 -o  boot.img
-
