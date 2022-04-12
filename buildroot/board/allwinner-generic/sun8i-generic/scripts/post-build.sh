3bd8e387f0912a1dc036173e7a6d3b1e36696b56
YuzukiTsuru <gloomyghost@gloomyghost.com>
2022-04-12 12:18:46 +0800

    merge dragonsecboot to pub

--- buildroot/board/allwinner-generic/sun8i-generic/scripts/post-build.sh
+++ buildroot/board/allwinner-generic/sun8i-generic/scripts/post-build.sh
@@ -12,5 +12,5 @@ cd $BINARIES_DIR
 echo "item=dtb, $5" >> boot_package.cfg
 
 $BINARIES_DIR/dragonsecboot  -pack boot_package.cfg
-mkbootimg --kernel  Image  --ramdisk  ramdisk.img --board sun20i --base  0x40200000 --kernel_offset  0x0 --ramdisk_offset  0x01000000 -o  boot.img
+mkbootimg --kernel zImage  --ramdisk  ramdisk.img --board sun8iw20p1 --base  0x40200000 --kernel_offset  0x0 --ramdisk_offset  0x01000000 -o  boot.img
 
