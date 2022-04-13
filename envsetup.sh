a9ee0fbf2967ddd9c9fda32809bfa55ca0dee2d8
YuzukiTsuru <gloomyghost@gloomyghost.com>
2022-04-13 22:04:31 +0800

    fix uboot patch issue

--- envsetup.sh
+++ envsetup.sh
@@ -99,6 +99,7 @@ alias rebuild_kernel="touch ./output/images/a.dtb && rm ./output/images/*.dtb &&
 alias rebuild_uboot="make uboot-rebuild -j8 && make"
 alias wsl_path="export PATH=\"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/wsl/lib\""
 alias sync_kernel="rm -rf output/build/linux* && rm -rf output/build/.linux* && make"
+alias sync_uboot="rm -rf output/build/uboot* && make"
 
 alias mm="make"
 alias m="make"
