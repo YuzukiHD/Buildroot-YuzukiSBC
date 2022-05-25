a7f36afa854feedf77314a3a4461c64da673c61d
YuzukiTsuru <gloomyghost@gloomyghost.com>
2022-05-25 16:50:40 +0800

    Support custom uboot cfg

--- buildroot/board/allwinner-generic/sun20i-generic/script/post-build.sh
+++ buildroot/board/allwinner-generic/sun20i-generic/script/post-build.sh
@@ -12,5 +12,6 @@ cd $BINARIES_DIR
 echo "item=dtb, $5" >> boot_package.cfg
 
 $BINARIES_DIR/dragonsecboot  -pack boot_package.cfg
+$BINARIES_DIR/mkenvimage -r -p 0x00 -s 131072 -o env.fex env.cfg
 mkbootimg --kernel  Image  --ramdisk  ramdisk.img --board sun20i --base  0x40200000 --kernel_offset  0x0 --ramdisk_offset  0x01000000 -o  boot.img
 
--- buildroot/board/allwinner-generic/sun8i-generic/scripts/post-build.sh
+++ buildroot/board/allwinner-generic/sun8i-generic/scripts/post-build.sh
@@ -12,6 +12,6 @@ cd $BINARIES_DIR
 echo "item=dtb, $5" >> boot_package.cfg
 
 $BINARIES_DIR/dragonsecboot  -pack boot_package.cfg
-$BINARIES_DIR/mkenvimage -r -p 0x00 -s 128k -o env.fex env.cfg
+$BINARIES_DIR/mkenvimage -r -p 0x00 -s 131072 -o env.fex env.cfg
 
 mkbootimg --kernel zImage  --ramdisk  ramdisk.img --board sun8iw20p1 --base  0x40200000 --kernel_offset  0x0 --ramdisk_offset  0x01000000 -o  boot.img
